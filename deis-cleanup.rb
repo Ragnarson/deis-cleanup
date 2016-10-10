72c7ad71d28d45febee0a7b34d19e45805bda668
Maciej Malecki <smt116@gmail.com>
2016-10-10 10:36:55 +0200

    Show progress during cleanup process
    the cleanup procedure may be very time consuming. It is hard
    to guess what is the current state and how many images left.
    
    Print simple message after each 10 containers / images.
    
    [#126870579]

--- deis-cleanup.rb
+++ deis-cleanup.rb
@@ -12,6 +12,12 @@ require "open3"
 require "csv"
 require "yaml"
 
+class String
+  def green
+    "\e[32m#{self}\e[0m"
+  end
+end
+
 def execute(command, abort_on_failure = false)
   output, status = Open3.capture2e(command)
   abort output if abort_on_failure && !status.success?
@@ -23,48 +29,43 @@ DRY_RUN = YAML.load(ENV.fetch("DRY_RUN", "false"))
 EXCLUDE_REGEX = /#{ENV.fetch("EXCLUDE_REGEX", "alpine|deis|blackhole|none|datadog|cleanup|heroku|python")}/
 CLEANUP_CONTAINERS = YAML.load(ENV.fetch("CLEANUP_CONTAINERS", "true"))
 
-remove_image = lambda do |image|
-  command = "docker rmi #{image[:repository]}:#{image[:tag]}"
-  command.prepend "echo " if DRY_RUN
-  puts execute(command)
-end
-
-remove_container = lambda do |container|
-  command = "docker rm #{container[:id]}"
-  command.prepend "echo #{container[:name]}: " if DRY_RUN
-  puts execute(command)
-end
-
 output = execute("docker images", true)
 headers, *rows = CSV.parse(output, col_sep: " ").map { |row| row.first(2) }
 headers.map! { |header| header.downcase.to_sym }
 images = rows.map { |row| headers.zip(row).to_h }
 
-puts "deis-cleanup started."
-
 if CLEANUP_CONTAINERS
-  puts "removing containers"
-
   output = execute("docker ps -a --format '{{.ID}};{{.Names}}' --filter 'exited=0'", true)
-  rows = CSV.parse(output, col_sep: ";")
-  rows.map { |row| %i(id name).zip(row).to_h }.reject { |container| container[:name].match(EXCLUDE_REGEX) }.
-    each(&remove_container)
+  containers = CSV.parse(output, col_sep: ";").
+    map    { |row| %i(id name).zip(row).to_h }.
+    reject { |container| container[:name].match(EXCLUDE_REGEX) }
+
+  containers.each_with_index do |container, index|
+    puts "Removing container ##{index + 1} (#{containers.count - index - 1} left).".green if (index % 10).zero?
+    command = "docker rm #{container[:id]}"
+    command.prepend("echo #{container[:name]}: ") if DRY_RUN
+    puts execute(command)
+  end
 end
 
 # Deis use two types of image tags - git-<sha> and v<version>. All git-tagged
 # images should be removed, in case of version tag we keep KEEP_LAST_VERSIONS
 # last images,
-puts "removing images"
-filtered = images.reject { |image| image[:repository].match(EXCLUDE_REGEX) }.
+filtered = images.
+  reject { |image| image[:repository].match(EXCLUDE_REGEX) }.
   group_by { |image| image[:tag].include?("git") ? :git : :versioned }
 
-%i(git versioned).each do |type|
-  filtered[type] ||= {}
-end
+useless_images = filtered.fetch(:versioned, {}).
+  group_by { |image| image[:repository] }.
+  map      { |_, images| images.sort_by { |image| image[:tag].split("v").last.to_i }[0..-(KEEP_LAST_VERSIONS + 1)] }.
+  concat(filtered.fetch(:git, [])).
+  flatten
 
-filtered[:git].each(&remove_image)
-filtered[:versioned].group_by { |image| image[:repository] }.each do |_, repository|
-  repository.sort_by { |e| e[:tag].split("v").last.to_i }[0..-(KEEP_LAST_VERSIONS + 1)].each(&remove_image)
+useless_images.each_with_index do |image, index|
+  puts "Removing image ##{index + 1} (#{useless_images.count - index - 1} left).".green if (index % 10).zero?
+  command = "docker rmi #{image[:repository]}:#{image[:tag]}"
+  command.prepend("echo ") if DRY_RUN
+  puts execute(command)
 end
 
 puts "deis-cleanup finished."
